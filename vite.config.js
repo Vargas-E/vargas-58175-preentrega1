3e7c6cbf6cba6f8169432250a8dd42aa64013980
Vargas-E <vargasivanezequiel@gmail.com>
2023-11-02 00:14:22 -0300

    git ignore restaurado. base en vite eliminada

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:  '/vargas-ivan-58175-react-coderhouse-preentrega1/' 
 })
